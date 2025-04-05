05b3073b1d3efaf92e6147c79f09c12b620d0ea8
mehmetiarmend680 <mehmetiarmend680@gmail.com>
2025-04-05 05:42:55 +0000

    Add 26.rs

---
+++ src/26.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Define variables
+    let x = 10;
+    let y = 5;
+
+    // Perform calculations
+    let result = x + y;
+
+    // Print the result
+    println!("The sum of {} and {} is {}", x, y, result);
+}
